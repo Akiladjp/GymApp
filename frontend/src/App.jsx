e026e64b5a5773c3b9092314667dd7a220fe2309
Akila Piyumal <92683626+Akiladjp@users.noreply.github.com>
2024-07-12 04:37:20 +0530

    1st commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Hero } from './components/Hero/Hero'
+import { Programs } from './components/Programs/Programs'
+
+function App() {
+
+  return (
+    <div className="App">
+          <Hero/>
+          <Programs/>
+    </div>
+  )
+}
+
+export default App
